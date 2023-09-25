89bcc469757ddd92d8b3bbd34818de253481db99
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-09-25 20:35:52 +0200

    render User component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,10 @@
-function User() {
-    return {
-
-    };
+function User(props) {
+    return (
+        <span className="user">
+            <span className="name">{props.name}</span>
+            <span className="handle">@{props.handle}</span>
+        </span>
+    );
 }
 
 export default User;
